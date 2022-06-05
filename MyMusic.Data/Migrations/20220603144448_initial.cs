1f70c4c0900e23daed61d0044d0fb61bd5e4c69b
johnmchale <38525955+johnmchale@users.noreply.github.com>
2022-06-05 20:04:17 +0100

    Add project files.

---
+++ MyMusic.Data/Migrations/20220603144448_initial.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Oracle.EntityFrameworkCore.Metadata;
+
+namespace MyMusic.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "JOHNMC");
+
+            migrationBuilder.CreateTable(
+                name: "ARTISTS",
+                schema: "JOHNMC",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    NAME = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ARTISTS", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MUSICS",
+                schema: "JOHNMC",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Oracle:ValueGenerationStrategy", OracleValueGenerationStrategy.IdentityColumn),
+                    NAME = table.Column<string>(maxLength: 50, nullable: false),
+                    ARTISTID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MUSICS", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_MUSICS_ARTISTS_ARTISTID",
+                        column: x => x.ARTISTID,
+                        principalSchema: "JOHNMC",
+                        principalTable: "ARTISTS",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MUSICS_ARTISTID",
+                schema: "JOHNMC",
+                table: "MUSICS",
+                column: "ARTISTID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MUSICS",
+                schema: "JOHNMC");
+
+            migrationBuilder.DropTable(
+                name: "ARTISTS",
+                schema: "JOHNMC");
+        }
+    }
+}
---
+++ MyMusic.Data/MyMusicDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMusic.Core.Models;
+using MyMusic.Data.Configurations;
+
+namespace MyMusic.Data
+{
+    public class MyMusicDbContext : DbContext
+    {
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        public MyMusicDbContext(DbContextOptions<MyMusicDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                // Replace table names
+                entityType.SetTableName(entityType.GetTableName().ToUpper());
+
+                // Replace column names
+                foreach (var property in entityType.GetProperties())
+                {
+                    property.SetColumnName(property.GetColumnName().ToUpper());
+                }
+
+                // Replace key names
+                foreach (var key in entityType.GetKeys())
+                {
+                    key.SetName(key.GetName().ToUpper());
+                }
+
+                // Replace foreign key names
+                foreach (var foreignKey in entityType.GetForeignKeys())
+                {
+                    foreignKey.SetConstraintName(foreignKey.GetConstraintName().ToUpper());
+                }
+
+                // Replace index names
+                foreach (var index in entityType.GetIndexes())
+                {
+                    index.SetName(index.GetName().ToUpper());
+                }
+            }
+
+            builder.HasDefaultSchema("JOHNMC");
+
+            builder
+                .ApplyConfiguration(new MusicConfiguration());
+
+            builder
+                .ApplyConfiguration(new ArtistConfiguration());
+        }
+    }
+}
